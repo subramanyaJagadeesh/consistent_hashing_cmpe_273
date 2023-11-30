aa53c334a3f0ee579f4786c32b3f913a90e695c6
Vineet Jaywant <vineet.jaywant@sjsu.edu>
2023-11-30 01:31:48 -0800

    Add store_key.

--- apache_server_2.py
+++ apache_server_2.py
@@ -36,6 +36,21 @@ class FlightServer(fl.FlightServerBase):
             # Respond to health check requests
             print("Server-1")
             return fl.RecordBatchStream(pa.RecordBatch.from_pandas(pd.DataFrame({'response': ['healthy']})))
+        
+    def store_key(self, context, descriptor, reader, writer):
+        # Read the incoming data from the gateway
+        batch = reader.read_next_batch()
+
+        # Store the data in memory using a unique key
+        key = f"{descriptor.path}_{context.peer()}"
+        self._data_store[key] = batch
+
+        # Respond to the gateway
+        schema = pa.schema([('response', pa.string())])
+        response_data = pa.array(['Data stored successfully'], type=pa.string())
+        response_batch = pa.RecordBatch.from_pandas(pd.DataFrame({'response': response_data}))
+        writer.write_batch(response_batch)
+        writer.close()
 
 server = fl.FlightServer()
 server.start(location)
