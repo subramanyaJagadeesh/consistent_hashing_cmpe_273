8e5512bc20fbe48a2089da2089c897333aa657d4
subramanyajagadeesh <subramanya.grj@gmail.com>
2023-12-02 13:27:00 -0800

    Fixed 3 servers

--- apache_server_2.py
+++ apache_server_2.py
@@ -2,56 +2,52 @@ import pathlib
 import pyarrow as pa
 import pyarrow.flight as fl
 import pandas as pd
-
-# Define a Flight endpoint
-location = fl.Location.for_grpc_tcp('localhost', 5000)
-
-# Define a FlightInfo object with schema and data
-schema = pa.schema()
-
-data = [
-    pa.array([1, 2, 3]),
-    pa.array(['Alice', 'Bob', 'Charlie'])
-]
-
-batch = pa.record_batch(data, schema)
-flight_info = fl.FlightInfo(schema)
+import pickle
+from company import Company
 
 # Define a Flight endpoint to serve the FlightInfo and RecordBatch
 class FlightServer(fl.FlightServerBase):
-    def __init__(self, location="grpc://0.0.0.0:8815", repo=pathlib.Path("./datasets"), **kwargs):
-        super(FlightServer, self).__init__(location, **kwargs)
+    def __init__(self, location, repo=pathlib.Path("./datasets"), **kwargs):
+        super(FlightServer, self).__init__(location)
         self._location = location
         self._repo = repo
-        self._data_store = {}
-        
-    def list_flights(self, context, criteria):
-        return [fl.FlightEndpoint("example", [flight_info])]
-
-    def do_get(self, context, ticket):
-        return fl.RecordBatchStream(batch)
+        self._data_store = {'12': Company('12', 'name')}
     
-    def health_check(self, context, ticket):
-        if ticket == b"health-check":
-            # Respond to health check requests
-            print("Server-1")
-            return fl.RecordBatchStream(pa.RecordBatch.from_pandas(pd.DataFrame({'response': ['healthy']})))
-        
-    def store_key(self, context, descriptor, reader, writer):
-        # Read the incoming data from the gateway
-        batch = reader.read_next_batch()
+    #Mainly used for checking server health
+    def do_action(self, context, action):
+        if action.type == 'health_check':
+            # Perform the health check
+            yield fl.Result(b'Server is healthy')
+        else:
+            raise fl.FlightUnimplementedError(f'Unknown action: {action.type}')
+
+    #Used to get the key from data store
+    def do_get(self, context, ticket):
+        key = ticket.ticket.decode('utf-8')
+        py_obj = self._data_store[key]
+        serialized_obj = pickle.dumps(py_obj)
+        # Create a PyArrow Table with a single binary column
+        data = pa.Table.from_arrays([pa.array([serialized_obj], type=pa.binary())], names=['data'])
+        return fl.RecordBatchStream(data)
+    
+    #Used to store the key in data store
+    def do_put(self, context, descriptor, reader, writer):
+        table = reader.read_all()
+        # Extract the binary data (assuming it's in a column named 'data')
+        serialized_obj = table.column('data').to_pylist()[0]
+        # Deserialize the Python object
+        py_obj = pickle.loads(serialized_obj)
 
         # Store the data in memory using a unique key
-        key = f"{descriptor.path}_{context.peer()}"
-        self._data_store[key] = batch
+        self._data_store[py_obj['id']] = py_obj
 
         # Respond to the gateway
-        schema = pa.schema([('response', pa.string())])
         response_data = pa.array(['Data stored successfully'], type=pa.string())
         response_batch = pa.RecordBatch.from_pandas(pd.DataFrame({'response': response_data}))
         writer.write_batch(response_batch)
         writer.close()
 
-server = fl.FlightServer()
-server.start(location)
+server = FlightServer("grpc://localhost:8817")
 print("Starting server2...")
+
+server.serve()
